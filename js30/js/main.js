06aced172ed9a12d823c96c139c5e20e7cd62142
Bukeif <az4659811@gmail.com>
2024-03-11 01:12:03 +0800

    updata js30/all javascript

--- js30/js/main.js
+++ js30/js/main.js
@@ -14,24 +14,3 @@ const showMenu = (toggleId, navId) =>{
 
 showMenu('nav--toggle','nav__menu');
 
-function redropdownMenu(){
-    const dropMenu = 
-    document.querySelectorAll(".dropdown--menu");
-    const dropSubMenu = 
-        document.querySelectorAll(".dropdown--submenu");
-        
-    dropMenu.forEach((menu) => {
-        const menuRect = menu.getBoundingClientRect();
-        if (menuRect.right > window.innerWidth)
-        menu.style.right = 0;
-    });
-    dropSubMenu.forEach((subMenu) => {
-        const subMenuRect = subMenu.getBoundingClientRect();
-        if (subMenuRect.right > window.innerWidth)
-        subMenu.style.left = `${-subMenuRect.width}px`;
-    });
-}
-redropdownMenu();
-window.addEventListener('resize', () =>{
-    redropdownMenu();
-})
